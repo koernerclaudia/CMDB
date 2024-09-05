7e2e6c1cafd083417a1bf6f18fcfbb1ca8e7e682
Claudia Koerner <claudia.koerner@gmail.com>
2024-09-05 10:37:48 +0200

    Update movies.js

--- movies.js
+++ movies.js
@@ -131,7 +131,6 @@ async (req, res) => {
   if (req.body.username) updateData.username = req.body.username;
   if (req.body.password) updateData.password = Users.hashPassword(req.body.password);
   if (req.body.email) updateData.email = req.body.email;
-  if (req.body.Birthday) updateData.Birthday = req.body.Birthday;
 
   // If no fields to update, return an error
   if (Object.keys(updateData).length === 0) {
@@ -154,89 +153,6 @@ async (req, res) => {
 
 });
 
-
-// app.put(
-//   '/users/:Username',
-//   passport.authenticate('jwt', { session: false }),
-//   [
-//     check('username', 'Username is required').isLength({ min: 5 }),
-//     check(
-//       'username',
-//       'username contains non alphanumeric characters - not allowed.'
-//     ).isAlphanumeric(),
-//     check('password', 'Password is required').not().isEmpty(),
-//     check('email', 'Email does not appear to be valid').isEmail(),
-//   ],
-//   async (req, res) => {
-//     // check the validation object for errors
-//     let errors = validationResult(req);
-
-//     if (!errors.isEmpty()) {
-//       return res.status(422).json({ errors: errors.array() });
-//     }
-//     // CONDITION TO CHECK ADDED HERE
-//     if (req.user.username !== req.params.username) {
-//       return res.status(400).send('Permission denied');
-//     }
-//     // CONDITION ENDS
-//     let hashedPassword = Users.hashPassword(req.body.password);
-//     await Users.findOneAndUpdate(
-//       { Username: req.params.username },
-//       {
-//         $set: {
-//           username: req.body.username,
-//           password: hashedPassword,
-//           email: req.body.email,
-//           Birthday: req.body.Birthday,
-//         },
-//       },
-//       { new: true }
-//     ) // This line makes sure that the updated document is returned
-//       .then((updatedUser) => {
-//         res.json(updatedUser);
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//         res.status(500).send('Error: ' + err);
-//       });
-//   }
-// );
-
-
-// app.put('/users/:username', passport.authenticate('jwt', { session: false }),
-// [
-//   check('username', 'Username is required').isLength({min: 5}),
-//   check('username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
-//   check('password', 'Password is required').not().isEmpty(),
-//   check('password', 'Password must be at least 8 characters long').isLength({ min: 8 }),
-//   check('email', 'Email does not appear to be valid').isEmail()
-// ],
-
-// async (req, res) => {
-//   // CONDITION TO CHECK ADDED HERE
-//   if(req.user.username !== req.params.username){
-//       return res.status(400).send('Permission denied');
-//   }
-//   // CONDITION ENDS
-//   let hashedPassword = Users.hashPassword(req.body.password);
-//   await Users.findOneAndUpdate({ username: req.params.username }, {
-//       $set:
-//       {
-//           username: req.body.username,
-//           password: hashedPassword,
-//           email: req.body.email,
-//           birthdate: req.body.Birthdate
-//       }
-//   },
-//       { new: true }) // This line makes sure that the updated document is returned
-//       .then((updatedUser) => {
-//           res.json(updatedUser);
-//       })
-//       .catch((err) => {
-//           console.log(err);
-//           res.status(500).send('Error: ' + err);
-//       })
-// });
 
 // Add a movie to a user's list of favorites
 // Useing addtoSet so it is only added once. In case already added,
